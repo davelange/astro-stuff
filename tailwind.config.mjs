e96839ccbdea3a8f3c7ee1bd3d9919af2e8e0394
David Lange <lange@finiam.com>
2023-10-19 09:48:35 +0100

    add comments

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -13,6 +13,9 @@ export default {
       dropShadow: {
         glow: "0 0 16px rgba(255,255,255, 0.25)",
       },
+      backgroundImage: {
+        grid: "url(\"data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 32 32' width='32' height='32' fill='none' stroke='rgb(148 163 184 / 0.05)'%3e%3cpath d='M0 .5H31.5V32'/%3e%3c/svg%3e\")'%3e%3cpath d='M0 .5H31.5V32'/%3e%3c/svg%3e)",
+      },
     },
   },
   plugins: [],
